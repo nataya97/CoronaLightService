73c88e6d1e1da67c53ae950b22ac1c84b899ab6b
PatMoKa <patrycja_kall@yahoo.com>
2021-01-17 21:19:54 +0100

    war added package - authorization

--- Frontend/corona-light-service/src/main.js
+++ Frontend/corona-light-service/src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+/*import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
@@ -15,3 +15,39 @@ const app = new Vue({
 
 app.config.$msalInstance = {}
 app.config.$emitter = require('tiny-emitter');
+*/
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+import authentication from './wrapper'
+
+Vue.config.productionTip = false
+
+/**new Vue({
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')**/
+
+
+Vue.config.productionTip = false
+
+// Init adal authentication - then create Vue app.
+authentication.initialize().then(_ => {
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    router,
+    vuetify,
+    template: '<App/>',
+    components: { App },
+    render: h => h(App),
+    var: _
+  });
+});
+
+//new Vue({ el: '#app', router, render: h => h(App) })
--- Frontend/corona-light-service/src/router/index.js
+++ Frontend/corona-light-service/src/router/index.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+/*import Vue from 'vue'
 import VueRouter from 'vue-router'
 import State from "../components/State";
 import District from "../components/District";
@@ -27,5 +27,74 @@ const routes = [
 const router = new VueRouter({
   routes,
 })
+
+export default router
+*/
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import State from "../components/State";
+import District from "../components/District";
+import authentication from '../wrapper'
+
+Vue.use(VueRouter)
+
+/*const routes = [
+  {
+    path: '/',
+    redirect: '/state',
+    name: 'State',
+    component: State,
+  },
+  {
+    path: '/state',
+    name: 'State',
+    component: State,
+  },
+  {
+    path: '/district',
+    name: 'District',
+    component: District,
+  }
+]*/
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'State',
+      component: State,
+      meta: {
+        requiresAuthentication: true
+      }
+    },
+    {
+      path: '/state',
+      name: 'State',
+      component: State,
+    },
+    {
+      path: '/district',
+      name: 'District',
+      component: District,
+    }
+  ]
+})
+
+// Global route guard
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuthentication)) {
+    // this route requires auth, check if logged in
+    if (authentication.isAuthenticated()) {
+      // only proceed if authenticated.
+      next();
+    } else {
+      authentication.signIn();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
